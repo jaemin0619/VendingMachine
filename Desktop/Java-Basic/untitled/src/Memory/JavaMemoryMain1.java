eac7d64d1c8d4c9a15f5836798f37e99b5a1d5c7
jaemin0619 <wjdwoals000619@naver.com>
2024-11-19 01:24:42 +0900

    java memory

---
+++ Desktop/Java-Basic/untitled/src/Memory/JavaMemoryMain1.java
@@ -0,0 +1,15 @@
+package Memory;
+
+public class JavaMemoryMain1 {
+    public static void main(String[] args) {
+
+    }
+    static void method1(int m1)
+    {
+
+    }
+    static void method2(int m2)
+    {
+
+    }
+}
---
+++ Desktop/Java-Basic/untitled/src/access/a/PublicClassInnerMain.java
@@ -0,0 +1,7 @@
+package access.a;
+
+public class PublicClassInnerMain {
+    publicClass publicClass=new publicClass();
+    DefaultClass1 class1=new DefaultClass1();
+    DefaultClass2 class2=new DefaultClass2();
+}
